004b8c6079d01adee12a3ad77d47728f2f1b5c51
Shiyao <liushiyao2014@gmail.com>
2016-07-19 21:42:16 -0500

    Repository on database

---
+++ src/com/bittiger/AdsSearch/repository/CustomRepository.java
@@ -0,0 +1,13 @@
+package com.bittiger.AdsSearch.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.bittiger.AdsSearch.model.Customer;
+
+public interface CustomRepository extends MongoRepository<Customer, String>{
+    public Customer findByFirstName(String firstName);
+    
+    public List<Customer> findByLastName(String lastName);
+}
