363b2c7e74fd5b7e604cef05fce01bf19cabd54b
Shiyao <liushiyao2014@gmail.com>
2016-08-09 23:46:49 -0500

    Modify AdsGenerator

--- src/com/bittiger/AdsSearch/service/generators/AdsGenerator.java
+++ src/com/bittiger/AdsSearch/service/generators/AdsGenerator.java
@@ -11,17 +11,8 @@ import com.bittiger.AdsSearch.model.Ad;
 
 @Service
 public class AdsGenerator extends FakeDataGenerator{
-    @Autowired
-    private CompanyGenerator companyGenerator;
     
-    private static final int CONTENT_MIN_LENGTH = 25;
-    private static final int CONTENT_MAX_LENGTH = 100;
     
-    private static final int TITLE_MIN_LENGTH = 10;
-    private static final int TITLE_MAX_LENGTH = 30;
     
-    private static final BigInteger DEFAULT_RANK = new BigInteger("0");
-    
-  
 
 }
