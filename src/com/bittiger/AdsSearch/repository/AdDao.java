4e55b558988d2d290fde2b0a9bb66f19634fae47
Shiyao <liushiyao2014@gmail.com>
2016-08-14 16:16:22 -0500

    Implement auto increment counter

--- src/com/bittiger/AdsSearch/repository/AdDao.java
+++ src/com/bittiger/AdsSearch/repository/AdDao.java
@@ -2,17 +2,29 @@ package com.bittiger.AdsSearch.repository;
 
 import java.util.List;
 
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 import com.bittiger.AdsSearch.model.Ad;
 import com.bittiger.AdsSearch.model.Compaign;
 import com.bittiger.AdsSearch.model.Company;
+import com.bittiger.AdsSearch.model.Counter;
 
 @Repository
 public class AdDao extends BaseDao{
     private static final String ID = "id";
+    
+    public Long incrementSeq(String collectionName) {
+        Counter counter = template.findAndModify(
+                new Query(Criteria.where(Counter.DOCUMENT_NAME).is(Counter.DOCUMENT_AD)), 
+                        new Update().inc("seq", 1),
+                        FindAndModifyOptions.options().returnNew(true),
+                        Counter.class);
+        return counter.getSeq();
+    }
     
     public void createAd(Ad newad) {
         newad.setAdId(this.incrementSeq("ad"));
--- src/com/bittiger/AdsSearch/repository/BaseDao.java
+++ src/com/bittiger/AdsSearch/repository/BaseDao.java
@@ -1,28 +1,12 @@
 package com.bittiger.AdsSearch.repository;
 
-import java.math.BigInteger;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
-
-import com.bittiger.AdsSearch.model.Counter;
 
 @Repository
 public class BaseDao {
     @Autowired
     protected MongoTemplate template;
     
-    public Long incrementSeq(String collectionName) {
-        Counter counter = template.findAndModify(
-                new Query(Criteria.where("_id").is(collectionName)), 
-                        new Update().inc("seq", 1),
-                        FindAndModifyOptions.options().returnNew(true),
-                        Counter.class);
-        return counter.getSeq();
-    }
 }
